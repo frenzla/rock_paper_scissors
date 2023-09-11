3a1999a88cc1a895acd6f27ac5eaa0ea29ec4aad
Frenzla <86185774+frenzla@users.noreply.github.com>
2023-09-11 22:11:28 +0200

    First round done

--- js/rps.js
+++ js/rps.js
@@ -12,19 +12,63 @@ console.log(randomInt);
 function getComputerChoice(numberForChoice) {
     switch (numberForChoice) {
         case 1:
-            return ("Rock");
+            return ("rock");
             break;
         case 2:
-            return ("Paper");
+            return ("paper");
             break;
         case 3:
-            return ("Scissors");
+            return ("scissors");
             break;
         default:
             alert ("Houston, we have a problem!");
     }
 }
-
 let computerChoice = getComputerChoice(randomInt);
-
 console.log(computerChoice);
+
+function getPlayerChoice() {
+    let getPlayerInput = prompt("Choose from Rock, Paper or Scissors!");
+    let resultPlayerInput = getPlayerInput.toLowerCase();
+    return resultPlayerInput;
+}
+
+let playerInput = getPlayerChoice();
+console.log(playerInput);
+
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection === computerSelection) {
+        let resultContest = "Ooops, it's a Tie... Try again!";
+        return resultContest;
+        }
+        else if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            let resultContest = "You Win! Rock beats Scissors!";
+            return resultContest;
+        } else {
+            let resultContest = "You Lose! Paper beats Rock!";
+            return resultContest;
+        }
+        }
+        else if (playerSelection === "paper") {
+            if (computerSelection === "scissors") {
+                let resultContest = "You Lose! Scissors beats Paper!";
+                return resultContest;
+            } else {
+                let resultContest = "You Win! Paper beats Rock!";
+                return resultContest;
+            }
+            }
+            else if (playerSelection === "scissors") {
+                if (computerSelection === "rock") {
+                    let resultContest = "You Lose! Rock beats Scissors!";
+                    return resultContest;
+                } else {
+                    let resultContest = "You Win! Scissors beats Paper!";
+                    return resultContest;
+                }
+                }
+            }
+
+let resultRound = playRound(playerInput,computerChoice);
+console.log(resultRound);
