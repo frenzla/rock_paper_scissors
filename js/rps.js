b1405640b909f72387b5142b6ce3c7f005130559
Frenzla <86185774+frenzla@users.noreply.github.com>
2023-09-11 19:56:24 +0200

    Computer choice ready

--- js/rps.js
+++ js/rps.js
@@ -1,1 +1,30 @@
-console.log("Hello world!");
+//My javascript file
+
+
+
+function getRandomInt(min = 1 , max = 3) {
+    return Math.floor(Math.random() * (max-min+1) + min);
+}
+
+let randomInt = getRandomInt();
+console.log(randomInt);
+
+function getComputerChoice(numberForChoice) {
+    switch (numberForChoice) {
+        case 1:
+            return ("Rock");
+            break;
+        case 2:
+            return ("Paper");
+            break;
+        case 3:
+            return ("Scissors");
+            break;
+        default:
+            alert ("Houston, we have a problem!");
+    }
+}
+
+let computerChoice = getComputerChoice(randomInt);
+
+console.log(computerChoice);
